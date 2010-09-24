cbca87b04f33d5313c70c9406518114706be0e38
Isaac Liu <isaacliu@dhcp-39-208.eecs.berkeley.edu>
2010-09-24 12:56:43 -0700

    adding a new rails project to repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysiteExamples::Application.config.secret_token = 'df047877203a57c82ee874273fae2a944302c65965bc550bffcc7f7ec75d707c0b51f9d74269276f81bd629fa31a16bfa1ca9558e0a5db88e1f730537efb54d5'
