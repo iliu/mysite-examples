74664938bdb088e74ddbe998693e2dd5cda74d11
Isaac Liu <isaacliu@isaacmac.(none)>
2010-09-24 19:27:27 -0700

    modified controller and view to pull from database instead of facebook graph api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,20 @@
 class HomeController < ApplicationController
   def index
     #Load facebook.yml info
-    config = YAML::load(File.open("#{RAILS_ROOT}/config/facebook.yml"));
+    #config = YAML::load(File.open("#{RAILS_ROOT}/config/facebook.yml"));
 
     #Instantiate a new application with our app_id so we can get an access token
-    my_app = FbGraph::Application.new(config['production']['app_id']);
+    #my_app = FbGraph::Application.new(config['production']['app_id']);
 
     #Actually get the access token
-    acc_tok = my_app.get_access_token(config['production']['client_secret']);
+    #acc_tok = my_app.get_access_token(config['production']['client_secret']);
 
     #Instantiate a new page class using the page_id specified 
-    @page = FbGraph::Page.new(config['production']['page_id'], :access_token => acc_tok).fetch;
+    #@page = FbGraph::Page.new(config['production']['page_id'], :access_token => acc_tok).fetch;
 
     #Grab the events from the page 
-    events = @page.events.sort_by{|e| e.start_time};
+    #events = @page.events.sort_by{|e| e.start_time};
+    events = FacebookEvent.find(:all)
     
     #Get the events that are upcoming
     @upcoming_events = events.find_all{|e| e.start_time >= Time.now};
